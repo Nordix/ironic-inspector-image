c22b7eb627d33281afdc6f3a89649b181f027b95
Nam Xuan Nguyen <nam.xuan.nguyen@est.tech>
2021-01-12 15:25:19 +0200

    Uplift

--- prepare-image.sh
+++ prepare-image.sh
@@ -2,10 +2,11 @@
 
 set -ex
  
-dnf install -y python3 python3-requests
+dnf install -y python3 python3-requests 
 curl https://raw.githubusercontent.com/openstack/tripleo-repos/master/tripleo_repos/main.py | python3 - -b master current-tripleo
 dnf upgrade -y
 dnf install -y $(cat /tmp/main-packages-list.txt)
+pip3 install --yes git+git://github.com/namnx228/ironic-inspector.git#egg=ironic-inspector
 mkdir -p /var/lib/ironic-inspector
 sqlite3 /var/lib/ironic-inspector/ironic-inspector.db "pragma journal_mode=wal"
 dnf remove -y sqlite
