579dba209ec361f430ff293ea3bc138d87b56883
Nam Xuan Nguyen <nam.xuan.nguyen@est.tech>
2021-01-12 15:54:44 +0200

    add git

--- prepare-image.sh
+++ prepare-image.sh
@@ -6,6 +6,7 @@ dnf install -y python3 python3-requests
 curl https://raw.githubusercontent.com/openstack/tripleo-repos/master/tripleo_repos/main.py | python3 - -b master current-tripleo
 dnf upgrade -y
 dnf install -y $(cat /tmp/main-packages-list.txt)
+pip3 install wheel
 pip3 install git+git://github.com/namnx228/ironic-inspector.git#egg=ironic-inspector
 mkdir -p /var/lib/ironic-inspector
 sqlite3 /var/lib/ironic-inspector/ironic-inspector.db "pragma journal_mode=wal"
